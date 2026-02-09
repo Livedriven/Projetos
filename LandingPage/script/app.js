7b52df4b47f4f6d986c2099dbc5a7d7cc2c94adb
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2026-02-08 22:25:02 -0300

    Delete LandingPage directory

--- LandingPage/script/app.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('menuToggle');
-const nav = document.getElementById('navLink');
-
-toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-})
